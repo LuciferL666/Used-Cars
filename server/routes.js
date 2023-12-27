a17b954d9f1e8212c47b2c757c0e056abd555e13
PeykoGeorgiev <peyko92gm@gmail.com>
2023-12-27 23:28:24 +0100

    Update routes.js

--- server/routes.js
+++ server/routes.js
@@ -4,6 +4,6 @@ const userController = require('./controllers/userController');
 const carController = require('./controllers/carController');
 
 router.use('/users', userController);
-router.use('/cars', carController);
+router.use('/data/cars', carController);  //if start with data if not remove data!
 
 module.exports = router;
