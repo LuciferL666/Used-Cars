8e937889fbd789b7617f63670cc7d2a95ea2a221
PeykoGeorgiev <peyko92gm@gmail.com>
2023-12-27 23:21:58 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,6 +8,6 @@ app.get('/', (req, res) => {
     res.send('RESTful service');
 });
 
-app.use(routes);
+app.use('/data', routes); //if start with data if not remove data!
 
 app.listen(3030, () => console.log('server is listening on port 3030...'));
