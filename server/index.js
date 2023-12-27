21ebfc3f571a19bfc5fe693a976cbe5097bc3892
PeykoGeorgiev <peyko92gm@gmail.com>
2023-12-27 23:18:22 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 
+const routes = require('./routes');
+
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('hello world');
+    res.send('RESTful service');
 });
+
+app.use(routes);
 
 app.listen(3030, () => console.log('server is listening on port 3030...'));
