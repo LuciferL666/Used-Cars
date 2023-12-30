5928478dca4acd0f4f742ca3aba23451a5deb4b2
PeykoGeorgiev <peyko92gm@gmail.com>
2023-12-30 23:18:51 +0100

    carSchema

--- server/models/Car.js
+++ server/models/Car.js
@@ -1,6 +1,51 @@
 const mongoose = require("mongoose");
 
-const carSchema = new mongoose.Schema({});
+const carSchema = new mongoose.Schema({
+    brand:{
+        type: String,
+        required: true
+    },
+    model:{
+        type: String,
+        required: true
+    },
+    year:{
+        type: String,
+        required: true
+    },
+    month:{
+        type: String,
+        required: true
+    },
+    body:{
+        type: String,
+        required: true
+    },
+    fuel:{
+        type: String,
+        required: true
+    },
+    transmission:{
+        type: String,
+        required: true
+    },
+    performance:{
+        type: String,
+        required: true
+    },
+    kilometers:{
+        type: String,
+        required: true
+    },
+    img:{
+        type: String,
+        required: true
+    },
+    price:{
+        type: String,
+        required: true
+    },
+});
 
 const Car = mongoose.model("Car", carSchema);
 
