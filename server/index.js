372e99d47ce291e7b4071790360927e7649d17ed
PeykoGeorgiev <peyko92gm@gmail.com>
2024-01-01 22:20:24 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,14 +7,17 @@ const app = express();
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
+//CORS
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Access-Control-Allow-Methods', '*')
   res.setHeader('Access-Control-Allow-Headers', '*')
-
+ 
   next();
 })
-
+//Or we can use middleware npm i cors
+//const cors = require('cors');
+//app.use(cors());
 app.get("/", (req, res) => {
   res.send("RESTful service");
 });
