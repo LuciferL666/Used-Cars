946feac4d963ba202df60aa82bad708685a47f37
PeykoGeorgiev <peyko92gm@gmail.com>
2024-01-02 23:27:20 +0100

    Update userController.js

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,20 +1,20 @@
 const router = require("express").Router();
 
-const userManager = require('../managers/userManager');
+const userManager = require("../managers/userManager");
 
-router.post('/register', async (req, res) => {
-    try {
-        console.log(req.body);
-        const user = await userManager.register(req.body);
-        
-        res.json({
-            email: '',
-            authToken: '',
-            userId: '',
-        })
-    } catch(err){
-        
-    }
+router.post("/register", async (req, res) => {
+  try {
+    console.log(req.body);
+    const user = await userManager.register(req.body);
+
+    res.json({
+      email: "",
+      authToken: "",
+      userId: "",
+    });
+  } catch (err) {
+    //2ЧАСА И 2 МИН ОСТАВАТ
+  }
 });
 
 module.exports = router;
