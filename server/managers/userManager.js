f83452b8469679130a8ff17788185c270252fe2e
PeykoGeorgiev <peyko92gm@gmail.com>
2024-01-05 23:28:53 +0100

    Update userManager.js

--- server/managers/userManager.js
+++ server/managers/userManager.js
@@ -1,5 +1,8 @@
-const User = require('../models/User');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const SECRET = 'c1b0f55c-8254-4a0b-8789-d780b25a060e';
+
+const User = require('../models/User');
 
 exports.register = (userData) => User.create(userData);
 
@@ -15,4 +18,13 @@ exports.login = async ({email, password}) => {
     if(!isValid) {
         throw new Error('Invalid username or password');
     }
+
+    const payload = {
+        _id: user._id,
+        email: user.email,
+    }
+
+    const token = jwt.sign(payload, SECRET, {expiresIn: '2d' });
+    
+    return token;
 };
