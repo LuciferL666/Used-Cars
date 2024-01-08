2a062569339cc41fb03928ef4cba683f75b6c8de
PeykoGeorgiev <peyko92gm@gmail.com>
2024-01-08 14:57:21 +0100

    create update function

--- server/managers/carManager.js
+++ server/managers/carManager.js
@@ -2,6 +2,8 @@ const Car = require('../models/Car');
 
 exports.getAll = () => Car.find();
 
-exports.getOne = (carId) => Car.findById(carId)
+exports.getOne = (carId) => Car.findById(carId);
 
-exports.create = (carData) => Car.create(carData)
+exports.create = (carData) => Car.create(carData);
+
+exports.update = (carId, carData) => Car.findByIdAndUpdate(carId, carData);
