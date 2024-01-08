fe6b50842fd2edff57cf59f2023b91107a51fbac
PeykoGeorgiev <peyko92gm@gmail.com>
2024-01-08 12:24:09 +0100

    Add owner ID for every created car

--- server/models/Car.js
+++ server/models/Car.js
@@ -45,6 +45,10 @@ const carSchema = new mongoose.Schema({
         type: String,
         required: true
     },
+    ownerId: {
+        type: mongoose.Types.ObjectId,
+        ref: 'User',
+    }
 });
 
 const Car = mongoose.model("Car", carSchema);
