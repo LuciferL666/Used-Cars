8f1c65c5772d30ad71f1e0ee6a3511bd426886d0
PeykoGeorgiev <peyko92gm@gmail.com>
2024-01-09 22:46:16 +0100

    Update carController.js

--- server/controllers/carController.js
+++ server/controllers/carController.js
@@ -1,40 +1,39 @@
 const router = require("express").Router();
 
-const carManager = require('../managers/carManager');
+const carManager = require("../managers/carManager");
 
-router.get ('/', async (req, res) => {
-    const cars = await carManager.getAll();
+router.get("/", async (req, res) => {
+  const cars = await carManager.getAll();
 
-    res.json(cars);
-})
-
-router.post('/', async (req, res) => {
-    try {
-        await carManager.create({
-            ...req.body,
-            _ownerId: req.user._id,
-        });
-
-        res.status(204).end();
-    } catch(err) {
-        res.status(400).json({
-            message: err.message //or 'Cannot create Car'
-        });
-    }
+  res.json(cars);
 });
 
-router.get('/:carId', async (req, res) => {
-    const car = await carManager.getOne(req.params.carId);
+router.post("/", async (req, res) => {
+  try {
+    await carManager.create({
+      ...req.body,
+      _ownerId: req.user._id,
+    });
 
-    res.json(car);
-})
+    res.status(204).end();
+  } catch (err) {
+    res.status(400).json({
+      message: err.message, //or 'Cannot create Car'
+    });
+  }
+});
 
-router.put('/:carId', async (req, res) => {
-    await carManager.update(req.params.carId, req.body);
+router.get("/:carId", async (req, res) => {
+  const car = await carManager.getOne(req.params.carId);
 
-    res.status(204).end()
-})
+  res.json(car);
+});
+
+router.put("/:carId", async (req, res) => {
+  await carManager.update(req.params.carId, req.body);
+
+  res.status(204).end();
+});
 //1:01ostava
-
 
 module.exports = router;
