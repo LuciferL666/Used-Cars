7fb269e1bcd2e3985ed03938aa4824c76da68066
PeykoGeorgiev <peyko92gm@gmail.com>
2024-01-09 22:46:55 +0100

    Update Car.js

--- server/models/Car.js
+++ server/models/Car.js
@@ -1,54 +1,54 @@
 const mongoose = require("mongoose");
 
 const carSchema = new mongoose.Schema({
-    brand:{
-        type: String,
-        required: true
-    },
-    model:{
-        type: String,
-        required: true
-    },
-    year:{
-        type: String,
-        required: true
-    },
-    month:{
-        type: String,
-        required: true
-    },
-    body:{
-        type: String,
-        required: true
-    },
-    fuel:{
-        type: String,
-        required: true
-    },
-    transmission:{
-        type: String,
-        required: true
-    },
-    performance:{
-        type: String,
-        required: true
-    },
-    kilometers:{
-        type: String,
-        required: true
-    },
-    img:{
-        type: String,
-        required: true
-    },
-    price:{
-        type: String,
-        required: true
-    },
-    _ownerId: {
-        type: mongoose.Types.ObjectId,
-        ref: 'User',
-    }
+  brand: {
+    type: String,
+    required: true,
+  },
+  model: {
+    type: String,
+    required: true,
+  },
+  year: {
+    type: String,
+    required: true,
+  },
+  month: {
+    type: String,
+    required: true,
+  },
+  body: {
+    type: String,
+    required: true,
+  },
+  fuel: {
+    type: String,
+    required: true,
+  },
+  transmission: {
+    type: String,
+    required: true,
+  },
+  performance: {
+    type: String,
+    required: true,
+  },
+  kilometers: {
+    type: String,
+    required: true,
+  },
+  img: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: String,
+    required: true,
+  },
+  _ownerId: {
+    type: mongoose.Types.ObjectId,
+    ref: "User",
+  },
 });
 
 const Car = mongoose.model("Car", carSchema);
