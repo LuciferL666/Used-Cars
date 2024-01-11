e7c2a895f3dfb91cb2a00a8214606fe4801df744
PeykoGeorgiev <peyko92gm@gmail.com>
2024-01-12 00:11:30 +0100

    Update carManager.js

--- server/managers/carManager.js
+++ server/managers/carManager.js
@@ -1,13 +1,16 @@
 const Car = require("../models/Car");
 
 exports.getAll = async (qs) => {
+    let query = Car.find();
 
     if(qs.where) {
         let [fieldName, ownerId] = qs.where.split('=');
         ownerId = ownerId.replaceAll('"', '')
+        //query = query.find({_ownerId: ownerId});
+        query = query.where('_ownerId').eq(ownerId);
     }
 
-    const result = await Car.find();
+    const result = await query
 
     return result;
 }
