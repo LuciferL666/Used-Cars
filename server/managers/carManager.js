22583d7257a2604cec081fb48bf7000f8fdd195b
PeykoGeorgiev <peyko92gm@gmail.com>
2024-01-11 23:06:54 +0100

    Update carManager.js

--- server/managers/carManager.js
+++ server/managers/carManager.js
@@ -7,3 +7,5 @@ exports.getOne = (carId) => Car.findById(carId);
 exports.create = (carData) => Car.create(carData);
 
 exports.update = (carId, carData) => Car.findByIdAndUpdate(carId, carData);
+
+exports.delete = (carId) => Car.findByIdAndDelete(carId);
