eba911b5dc91d448793835cf7acf30aca296c3bc
PeykoGeorgiev <peyko92gm@gmail.com>
2024-02-04 15:26:00 +0100

    Create Header.jsx

---
+++ client/src/components/header/Header.jsx
@@ -0,0 +1,25 @@
+export default function Header() {
+    return(
+        <header>
+      <nav className="site-navigation">
+        <ul>
+          <li>
+            <a class="usedCars" href="/home">Used Cars</a>
+          </li>
+          <li>
+            <a href="/catalog">catalog</a>
+          </li>
+          <li>
+            <a href="/register">register</a>
+          </li>
+          <li>
+            <a href="/login">login</a>
+          </li>
+          <li>
+            <a href="/logout">logout</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+    )
+}
