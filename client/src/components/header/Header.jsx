dc2dfff169b8fab97a6513a4301bface24d96152
PeykoGeorgiev <peyko92gm@gmail.com>
2024-02-08 22:28:13 +0100

    Update Header.jsx

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -10,6 +10,7 @@ export default function Header() {
             <a href="/catalog">Catalog</a>
           </li>
           <li>
+            <a href="/register">Register</a>
           </li>
           <li>
             <a href="/login">login</a>
