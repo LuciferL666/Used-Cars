a3979c0538191cb0c122742d4e76f0346231e26e
PeykoGeorgiev <peyko92gm@gmail.com>
2024-02-11 21:44:25 +0100

    Add routes navigation
    add path to catalog

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,16 @@
+import { Routes, Route, } from "react-router-dom";
 import Header from "./components/header/Header"
+import Catalog from "./components/catalog/Catalog";
+
 
 function App() {
   return (
     <div id="box">
       <Header />
+
+      <Routes>
+        <Route path='/catalog' element={<Catalog />} />
+      </Routes>
     </div>
   )
 }
