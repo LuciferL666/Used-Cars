a3e3285cff1c03493bd6e2cac9b1a0eb167650cc
PeykoGeorgiev <peyko92gm@gmail.com>
2024-02-16 23:33:56 +0100

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route, } from "react-router-dom";
 import Header from "./components/header/Header"
 import Catalog from "./components/catalog/Catalog";
+import Login from "./components/login/Login";
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
 
       <Routes>
         <Route path='/catalog' element={<Catalog />} />
+        <Route path='/login' element={<Login />} />
       </Routes>
     </div>
   )
