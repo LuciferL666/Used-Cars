532216b6636a3e8090d36246ce18df4ece284816
PeykoGeorgiev <peyko92gm@gmail.com>
2024-02-16 23:34:16 +0100

    Update Login.jsx

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -1,5 +1,25 @@
 export default function Login() {
     return (
-        
+        <div class="login">
+        <h1>Login</h1>
+        <form action="">
+          <input
+            type="email"
+            class="email-login"
+            name="Email"
+            placeholder="Your Email"
+          />
+          <input
+            type="password"
+            class="password"
+            name="Password"
+            placeholder="Your Password"
+          />
+          <button class="submit-button" type="submit">Submit</button>
+          <button class="goToRegistration" type="button">
+            Don't have an account? Click here
+          </button>
+        </form>
+      </div>
     )
 }
