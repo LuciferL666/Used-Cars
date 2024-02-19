a5e85cfe30a760a623dc82b614fed5b0f682f0c9
PeykoGeorgiev <peyko92gm@gmail.com>
2024-02-19 22:52:02 +0100

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import Header from "./components/header/Header"
 import Catalog from "./components/catalog/Catalog";
 import Login from "./components/login/Login";
 import Register from "./components/register/Register";
+import Create from "./components/create/Create";
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
         <Route path='/catalog' element={<Catalog />} />
         <Route path='/login' element={<Login />} />
         <Route path='/register' element={<Register />} />
+        <Route path='/create' element={<Create />} />
 
       </Routes>
     </div>
