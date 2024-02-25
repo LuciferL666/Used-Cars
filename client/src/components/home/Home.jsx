a56df51fa092ed3c0a846f74c9c377f2dde94dbc
PeykoGeorgiev <peyko92gm@gmail.com>
2024-02-25 23:43:40 +0100

    Update Home.jsx

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -3,6 +3,7 @@ export default function Home() {
         <div className="homePage">
             <h1>Welcome to our Used Car Marketplace!</h1>
             <p>Sell your car with us today and find the best deals.</p>
+            <button>Sell Your Car</button>
         </div>
     )
 }
