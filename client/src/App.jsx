e8cb775526be4686f8dbc0882bb54bcd8a138783
PeykoGeorgiev <peyko92gm@gmail.com>
2024-02-27 22:06:48 +0100

    path to home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
       <Header />
 
       <Routes>
-        <Route path='/home' element={<Home />} />
+        <Route path='/' element={<Home />} />
         <Route path='/catalog' element={<Catalog />} />
         <Route path='/login' element={<Login />} />
         <Route path='/register' element={<Register />} />
