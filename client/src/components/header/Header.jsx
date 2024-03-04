30cf34b11082d89630cc2ee967a1c71fba5db77f
PeykoGeorgiev <peyko92gm@gmail.com>
2024-03-04 23:54:46 +0100

    Update Header.jsx

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom'
 export default function Header() {
     return(
         <header>
+            <h1><Link className="usedCars" to="/">Used Cars</Link></h1>
       <nav className="site-navigation">
         <ul>
           <li>
