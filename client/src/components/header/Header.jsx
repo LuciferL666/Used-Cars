f64f30d3b6bcd0bdab2abf465b277d3a4a3d8959
PeykoGeorgiev <peyko92gm@gmail.com>
2024-03-04 23:55:09 +0100

    Update Header.jsx

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -5,6 +5,18 @@ export default function Header() {
         <header>
             <h1><Link className="usedCars" to="/">Used Cars</Link></h1>
       <nav className="site-navigation">
+        
+            <Link to="/catalog">Catalog</Link>
+            
+            <div id='user'>
+            <Link to="/create">Create</Link>
+            <Link to="/logout">Logout</Link>
+            </div>
+
+            <div id='guest'>
+            <Link to="/register">Register</Link>
+            <Link to="/login">Login</Link>
+            </div>
       </nav>
     </header>
     )
